85d61d4f6f61e6de61446b09108ba97f243ce9f6
Hector Huertas <hectorhuertas@gmail.com>
2025-07-24 15:38:14 +0200

    test

--- baselinehealth_test.go
+++ baselinehealth_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestPartiallyFailingBaselineHealthCheck(t *testing.T) {
 	// "192.0.2.0" is a test ip according to https://www.rfc-editor.org/rfc/rfc5737#section-3
-	h := NewBaselineCheckWithAddresses([]string{"1.1.1.1", "192.0.2.0"})
+	h := NewBaselineCheckWithAddresses([]string{"localhost", "192.0.2.0"})
 	result := h.Check()
 	assert.Equal(t, result.healthy, true)
 	assert.Equal(t, result.err, "")
