cc6cfec463260bd1190bf2262792405cf57d61ed
Hector Huertas <hectorhuertas@gmail.com>
2025-07-30 11:36:11 +0200

    Stop reusing pingers for several checks
    Pingers should only execute Run() once. Reusing the pingers for several
    runs produce broken statistics.
    
    Running the following code for testing:
    ```go
    package main
    
    import (
    	"fmt"
    	"time"
    
    	probing "github.com/prometheus-community/pro-bing"
    )
    
    func main() {
    	reused, _ := probing.NewPinger("8.8.8.8")
    	reused.Count = 1
    
    	for {
    		renewed, _ := probing.NewPinger("8.8.8.8")
    		renewed.Count = 1
    
    		reused.Run()
    		renewed.Run()
    
    		fmt.Printf("reused: %+v\n", reused.Statistics())
    		fmt.Printf("renewed: %+v\n", renewed.Statistics())
    
    		time.Sleep(2 * time.Second)
    	}
    }
    ```
    
    Produces the following logs when 8.8.8.8 is reachable:
    ```
    reused: &{PacketsRecv:1 PacketsSent:1 PacketsRecvDuplicates:0 PacketLoss:0  ...
    renewed: &{PacketsRecv:1 PacketsSent:1 PacketsRecvDuplicates:0 PacketLoss:0 ...
    reused: &{PacketsRecv:1 PacketsSent:2 PacketsRecvDuplicates:0 PacketLoss:50 ...
    renewed: &{PacketsRecv:1 PacketsSent:1 PacketsRecvDuplicates:0 PacketLoss:0 ...
    reused: &{PacketsRecv:1 PacketsSent:3 PacketsRecvDuplicates:0 PacketLoss:66 ...
    renewed: &{PacketsRecv:1 PacketsSent:1 PacketsRecvDuplicates:0 PacketLoss:0 ...
    reused: &{PacketsRecv:1 PacketsSent:4 PacketsRecvDuplicates:0 PacketLoss:75 ...
    renewed: &{PacketsRecv:1 PacketsSent:1 PacketsRecvDuplicates:0 PacketLoss:0 ...
    reused: &{PacketsRecv:1 PacketsSent:5 PacketsRecvDuplicates:0 PacketLoss:80 ...
    ```
    Logs show that the reused pinger tracks the total packets sent, but
    fails to track(or receive) new packets, so PacketLoss cannot be used to
    determine a successful ping on subsequent runs.
    
    And when blocking traffic to 8.8.8.8 the following logs are produced:
    
    ```
    reused: &{PacketsRecv:1 PacketsSent:15 PacketsRecvDuplicates:0 PacketLoss:93 ...
    renewed: &{PacketsRecv:0 PacketsSent:0 PacketsRecvDuplicates:0 PacketLoss:0  ...
    reused: &{PacketsRecv:1 PacketsSent:15 PacketsRecvDuplicates:0 PacketLoss:93 ...
    renewed: &{PacketsRecv:0 PacketsSent:0 PacketsRecvDuplicates:0 PacketLoss:0  ...
    reused: &{PacketsRecv:1 PacketsSent:15 PacketsRecvDuplicates:0 PacketLoss:93 ...
    renewed: &{PacketsRecv:0 PacketsSent:0 PacketsRecvDuplicates:0 PacketLoss:0  ...
    ```
    Logs show that the reused pinger stops sending new packages, but keeps
    the previous received packet, so PacketsRecv cannot be used to determine
    a failed ping on subsequent runs.
    
    So the results of the reused probe are not reliable, thus the need for
    new probes for each check

--- pinghealth_test.go
+++ pinghealth_test.go
@@ -40,3 +40,12 @@ func TestFailingPingCheck(t *testing.T) {
 	assert.Equal(t, result.err, "")
 	assert.Equal(t, result.output, "192.0.2.0: 1 packets transmitted, 0 packets received, 100% packet loss, ")
 }
+func TestPingChecksAreRepeteable(t *testing.T) {
+	// "192.0.2.0" is a test ip according to https://www.rfc-editor.org/rfc/rfc5737#section-3
+	h := NewPingCheck([]string{"192.0.2.0"})
+	result := h.Check()
+	result = h.Check()
+	assert.Equal(t, result.healthy, false)
+	assert.Equal(t, result.err, "")
+	assert.Equal(t, result.output, "192.0.2.0: 1 packets transmitted, 0 packets received, 100% packet loss, ")
+}
