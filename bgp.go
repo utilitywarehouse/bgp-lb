17be7adfc716d227bb49fa6a73d033c37ce9c7e8
ffilippopoulos <ffilippopoulos@uw.co.uk>
2026-02-20 15:28:08 +0000

    Update GoBgp to v4.2.0

--- bgp.go
+++ bgp.go
@@ -4,11 +4,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/netip"
+	"time"
 
-	api "github.com/osrg/gobgp/v3/api"
-	gobgp "github.com/osrg/gobgp/v3/pkg/server"
+	api "github.com/osrg/gobgp/v4/api"
+	"github.com/osrg/gobgp/v4/pkg/apiutil"
+	"github.com/osrg/gobgp/v4/pkg/packet/bgp"
+	"github.com/osrg/gobgp/v4/pkg/server"
+	gobgp "github.com/osrg/gobgp/v4/pkg/server"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
-	apb "google.golang.org/protobuf/types/known/anypb"
 )
 
 var (
@@ -35,11 +40,12 @@ func initBgpServer(routerId string, asn uint32, listenPort int32) (*BgpServer, e
 	}
 
 	// monitor the change of the peer state
-	if err := s.WatchEvent(context.Background(), &api.WatchEventRequest{Peer: &api.WatchEventRequest_Peer{}}, func(r *api.WatchEventResponse) {
-		if p := r.GetPeer(); p != nil && p.Type == api.WatchEventResponse_PeerEvent_STATE {
-			log.Info(p)
-		}
-	}); err != nil {
+	if err := s.WatchEvent(context.Background(), server.WatchEventMessageCallbacks{
+		OnPeerUpdate: func(peer *apiutil.WatchEventMessage_PeerEvent, _ time.Time) {
+			if peer.Type == apiutil.PEER_EVENT_STATE {
+				log.Info(peer)
+			}
+		}}); err != nil {
 		log.Fatal(err)
 	}
 
@@ -56,27 +62,17 @@ func (bs *BgpServer) AddPeer(address string, asn uint32) error {
 	return bs.server.AddPeer(context.Background(), &api.AddPeerRequest{Peer: n})
 }
 
-func (bs *BgpServer) AddV4Path(prefix string, prefixLen uint32, nextHop string) error {
-	nlri, _ := apb.New(&api.IPAddressPrefix{
-		Prefix:    prefix,
-		PrefixLen: prefixLen,
-	})
+func (bs *BgpServer) AddV4Path(prefix string, prefixLen int, nextHop string) error {
+	path := fmt.Sprintf("%s/%d", prefix, prefixLen)
+	nlri, _ := bgp.NewIPAddrPrefix(netip.MustParsePrefix(path))
+	a1 := bgp.NewPathAttributeOrigin(0) // the prefix originates from an interior routing protocol (IGP)
+	a2, _ := bgp.NewPathAttributeNextHop(netip.MustParseAddr(nextHop))
+	attrs := []bgp.PathAttributeInterface{a1, a2}
 
-	a1, _ := apb.New(&api.OriginAttribute{
-		Origin: 0, // the prefix originates from an interior routing protocol (IGP)
-	})
-	a2, _ := apb.New(&api.NextHopAttribute{
-		NextHop: nextHop,
-	})
-	attrs := []*apb.Any{a1, a2}
-
-	_, err := bs.server.AddPath(context.Background(), &api.AddPathRequest{
-		Path: &api.Path{
-			Family: v4Family,
-			Nlri:   nlri,
-			Pattrs: attrs,
-		},
-	})
+	_, err := bs.server.AddPath(apiutil.AddPathRequest{Paths: []*apiutil.Path{{
+		Nlri:  nlri,
+		Attrs: attrs,
+	}}})
 	if err != nil {
 		return err
 	}
@@ -84,27 +80,17 @@ func (bs *BgpServer) AddV4Path(prefix string, prefixLen uint32, nextHop string) 
 	return nil
 }
 
-func (bs *BgpServer) DeleteV4Path(prefix string, prefixLen uint32, nextHop string) error {
-	nlri, _ := apb.New(&api.IPAddressPrefix{
-		Prefix:    prefix,
-		PrefixLen: prefixLen,
-	})
+func (bs *BgpServer) DeleteV4Path(prefix string, prefixLen int, nextHop string) error {
+	path := fmt.Sprintf("%s/%d", prefix, prefixLen)
+	nlri, _ := bgp.NewIPAddrPrefix(netip.MustParsePrefix(path))
+	a1 := bgp.NewPathAttributeOrigin(0) // the prefix originates from an interior routing protocol (IGP)
+	a2, _ := bgp.NewPathAttributeNextHop(netip.MustParseAddr(nextHop))
+	attrs := []bgp.PathAttributeInterface{a1, a2}
 
-	a1, _ := apb.New(&api.OriginAttribute{
-		Origin: 0, // the prefix originates from an interior routing protocol (IGP)
-	})
-	a2, _ := apb.New(&api.NextHopAttribute{
-		NextHop: nextHop,
-	})
-	attrs := []*apb.Any{a1, a2}
-
-	err := bs.server.DeletePath(context.Background(), &api.DeletePathRequest{
-		Path: &api.Path{
-			Family: v4Family,
-			Nlri:   nlri,
-			Pattrs: attrs,
-		},
-	})
+	err := bs.server.DeletePath(apiutil.DeletePathRequest{Paths: []*apiutil.Path{{
+		Nlri:  nlri,
+		Attrs: attrs,
+	}}})
 	if err != nil {
 		return err
 	}
@@ -113,8 +99,18 @@ func (bs *BgpServer) DeleteV4Path(prefix string, prefixLen uint32, nextHop strin
 }
 
 func (bs *BgpServer) ListV4Paths() {
-	bs.server.ListPath(context.Background(), &api.ListPathRequest{Family: v4Family}, func(p *api.Destination) {
-		log.Info(p)
+	bs.server.ListPath(apiutil.ListPathRequest{
+		TableType: api.TableType_TABLE_TYPE_GLOBAL,
+	}, func(prefix bgp.NLRI, paths []*apiutil.Path) {
+		log.Info(prefix.String())
+		for _, p := range paths {
+			log.WithFields(logrus.Fields{
+				"peer_asn":     p.PeerASN,
+				"peer_address": p.PeerAddress,
+				"age":          p.Age,
+				"best":         p.Best,
+			}).Info("path")
+		}
 	})
 }
 
--- go.mod
+++ go.mod
@@ -1,16 +1,15 @@
 module github.com/utilitywarehouse/bgp-lb
 
-go 1.24.5
+go 1.25.0
 
 require (
 	github.com/moby/ipvs v1.1.0
-	github.com/osrg/gobgp/v3 v3.37.0
-	github.com/prometheus-community/pro-bing v0.7.0
+	github.com/osrg/gobgp/v4 v4.2.0
+	github.com/prometheus-community/pro-bing v0.8.0
 	github.com/prometheus/client_golang v1.23.2
 	github.com/sirupsen/logrus v1.9.4
 	github.com/stretchr/testify v1.11.1
 	github.com/vishvananda/netlink v1.3.1
-	google.golang.org/protobuf v1.36.11
 )
 
 require (
@@ -21,30 +20,32 @@ require (
 	github.com/eapache/channels v1.1.0 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/k-sone/critbitgo v1.4.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
-	github.com/prometheus/common v0.66.1 // indirect
-	github.com/prometheus/procfs v0.17.0 // indirect
-	github.com/sagikazarmark/locafero v0.9.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.14.0 // indirect
-	github.com/spf13/cast v1.9.2 // indirect
-	github.com/spf13/pflag v1.0.7 // indirect
-	github.com/spf13/viper v1.20.1 // indirect
+	github.com/prometheus/common v0.67.5 // indirect
+	github.com/prometheus/procfs v0.19.2 // indirect
+	github.com/sagikazarmark/locafero v0.12.0 // indirect
+	github.com/segmentio/fasthash v1.0.3 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/spf13/viper v1.21.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/vishvananda/netns v0.0.5 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.yaml.in/yaml/v2 v2.4.2 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250721164621-a45f3dfb1074 // indirect
-	google.golang.org/grpc v1.74.2 // indirect
+	go.yaml.in/yaml/v2 v2.4.3 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/net v0.50.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260217215200-42d3e9bedb6d // indirect
+	google.golang.org/grpc v1.79.1 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
